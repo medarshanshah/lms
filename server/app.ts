4d5a5225ae4740ad98b95088d071e69322d7491e
Darshan <darshanshah.ds2017@gmail.com>
2024-10-26 20:44:13 +0545

    test request added

--- server/app.ts
+++ server/app.ts
@@ -1,2 +1,28 @@
+require("dotenv").config();
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 export const app = express();
+
+//body parser
+app.use(express.json({limit: "50mb"}))  
+
+//cookie parser
+app.use(cookieParser());
+
+//cors = cross origin resource sharing
+app.use(cors({origin: process.env.ORIGIN})); // from .env file ORIGIN = ['http.//localhost:3000']
+
+// requests
+app.get("/test", (req, res, next) => {
+    res.status(200).json({
+        success: true,
+        message: "Hello, World!"
+    })
+})
+
+app.all("*", (req, res, next) => {
+    const err = new Error(`Route ${req.originalUrl} not found`) as any;
+    err.statusCode = 404;
+    next(err);
+})
