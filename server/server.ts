b6dcd1f3d9443697066bbe5674300a3220e7a83e
Darshan <darshanshah.ds2017@gmail.com>
2024-10-26 20:44:26 +0545

    mongodb connection

--- server/server.ts
+++ server/server.ts
@@ -1,8 +1,10 @@
 import {app} from "./app";
+import connectDB from "./utils/db";
 
 require("dotenv").config();
 
 //create server
 app.listen(process.env.PORT, () => {
-    console.log(`Server is connected with the port ${process.env.PORT}`)
+    console.log(`Server is connected with the port ${process.env.PORT}`);
+    connectDB();
 })
